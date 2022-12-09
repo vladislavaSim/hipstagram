f5d8d775698f4fdd69f9f21de9445dcb65004059
vladislavaSim <simonova.vladislava@gmail.com>
2022-12-09 13:07:31 +0200

    error connection page updated

--- src/helpers/Preloader.jsx
+++ src/helpers/Preloader.jsx
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux';
 import loading from '../img/Loading_icon.gif'
 import React from "react";
+import ghost from '../img/ghost.gif'
 
 const Preloaded = ({ promiseName, promiseState, children }) => (
     <>
         {promiseState[promiseName]?.status === 'RESOLVED' ? (
             children
         ) : promiseState[promiseName]?.status === 'REJECTED' ? (
-            <h1 style={{color: 'red'}}>Error loading</h1>
+            <div>
+                <h1 style={{color: 'red'}}>Error loading :(</h1>
+                <h4>Please, check your internet connection</h4>
+                <img src={ghost} alt='error-gif'/>
+            </div>
         ) : (
             <img style={{display: 'block', margin: '0 auto', marginBottom: '200px', padding: '10px'}}
                  src={loading} width="400px" height="auto" alt='loading-gif'/>
@@ -16,5 +21,6 @@ const Preloaded = ({ promiseName, promiseState, children }) => (
 );
 
 export const CPreloaded = connect((state) => ({
-        promiseState: state.promise }
+        promiseState: state.promise
+}
 ))(Preloaded);
